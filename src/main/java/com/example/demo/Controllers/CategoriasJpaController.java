a9919bdaa207d6379d80ec674d0ef292aa9c6003
Eric Aldana <aldanatorreserick@gmail.com>
2022-10-20 10:03:18 -0500

    Se agraga rama con el paquete de controladores

--- src/main/java/com/example/demo/Controllers/CategoriasJpaController.java
+++ src/main/java/com/example/demo/Controllers/CategoriasJpaController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author aldan
  */
 @RestController()
-@RequestMapping("/categorias")
+@RequestMapping("/categorias") 
 public class CategoriasJpaController implements Serializable {
 
     public CategoriasJpaController(EntityManagerFactory emf) {
